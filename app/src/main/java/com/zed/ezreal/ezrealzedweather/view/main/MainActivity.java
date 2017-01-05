c0719d633ca950c0f1e1808ebc1ef90ad8533792
ezreal <ext.ezreal.cai@ef.com>
2017-01-05 17:06:27 +0800

    增加了 splash 以及动画,修复了3个问题

--- app/src/main/java/com/zed/ezreal/ezrealzedweather/view/main/MainActivity.java
+++ app/src/main/java/com/zed/ezreal/ezrealzedweather/view/main/MainActivity.java
@@ -1,15 +1,44 @@
 package com.zed.ezreal.ezrealzedweather.view.main;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 import com.zed.ezreal.ezrealzedweather.R;
+import com.zed.ezreal.ezrealzedweather.view.BaseActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
+
+    private long exitTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            exit();
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void exit() {
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            Toast.makeText(getApplicationContext(), "再按一次退出程序",
+                    Toast.LENGTH_SHORT).show();
+            exitTime = System.currentTimeMillis();
+        } else {
+            finish();
+            System.exit(0);
+        }
+    }
+
 }
